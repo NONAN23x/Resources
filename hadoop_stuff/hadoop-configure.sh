c65936ff1b04c3e23e2a5fcace74932fd9556aa0
Nomaan Jamaluddin <nomaanjamaluddin@gmail.com>
2023-12-15 20:25:55 +0530

    Add Hadoop Scripts

--- hadoop_stuff/hadoop-configure.sh
+++ hadoop_stuff/hadoop-configure.sh
@@ -20,6 +20,11 @@ HADOOP_HDFS_HOME=$HADOOP_HOME
 YARN_HOME=$HADOOP_HOME
 HADOOP_COMMON_LIB_NATIVE_DIR="$HADOOP_HOME/lib/native"
 PATH="$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin"
+HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop 
+HADOOP_OPTS="-Djava.library.path=$HADOOP_HOME/lib/native" 
+PDSH_RCMD_TYPE=ssh
+
+
 
 # Check if the .bashrc file is writable
 if [[ -w ~/.bashrc ]]; then
@@ -29,6 +34,9 @@ if [[ -w ~/.bashrc ]]; then
   echo "export HADOOP_MAPRED_HOME=$HADOOP_HOME" >> ~/.bashrc
   echo "export HADOOP_COMMON_HOME=$HADOOP_HOME" >> ~/.bashrc
   echo "export HADOOP_HDFS_HOME=$HADOOP_HOME" >> ~/.bashrc
+  echo "export HADOOP_CONF_DIR=$HADOOP_CONF_DIR" >> ~/.bashrc
+  echo "export HADOOP_OPTS=HADOOP_OPTS" >> ~/.bashrc
+  echo "export PDSH_RCMD_TYPE=$PDSH_RCMD_TYPE" >> ~/.bashrc
   echo "export YARN_HOME=$HADOOP_HOME" >> ~/.bashrc
   echo "export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native" >> ~/.bashrc
   echo "export PATH=$PATH" >> ~/.bashrc
@@ -40,73 +48,86 @@ fi
 ### Configuring Apache Hadoop
 
 hadoop_env_content="export JAVA_HOME="/usr/lib/jvm/default-java/""  
-hadoop_class_path="export HADOOP_CLASSPATH+="'$HADOOP_HOME/lib/*.jar'""
+hadoop_class_path="export HADOOP_CLASSPATH+="' $HADOOP_HOME/lib/*.jar'""
 sudo echo $hadoop_env_content > $HADOOP_HOME/etc/hadoop/hadoop-env.sh
 sudo echo $hadoop_class_path >> $HADOOP_HOME/etc/hadoop/hadoop-env.sh
 
-xml_content="<configuration> <property>
 
-      <name>fs.default.name</name>
-
-      <value>hdfs://0.0.0.0:9000</value>
-
-      <description>The default file system URI</description>
-
-   </property> </configuration>"
+xml_content="<configuration> 
+ <property> 
+ <name>fs.defaultFS</name> 
+ <value>hdfs://localhost:9000</value>  </property> 
+ <property> 
+<name>hadoop.proxyuser.dataflair.groups</name> <value>*</value> 
+ </property> 
+ <property> 
+<name>hadoop.proxyuser.dataflair.hosts</name> <value>*</value> 
+ </property> 
+ <property> 
+<name>hadoop.proxyuser.server.hosts</name> <value>*</value> 
+ </property> 
+ <property> 
+<name>hadoop.proxyuser.server.groups</name> <value>*</value> 
+ </property> 
+</configuration>"
 sudo echo $xml_content > $HADOOP_HOME/etc/hadoop/core-site.xml
+echo "Configured core-site.xml"
+
 
 sudo mkdir -p /home/hadoop/hdfs/{namenode,datanode}
 sudo chown -R hadoop:hadoop /home/hadoop/hdfs
 
+
 hdfs_site_content="<configuration> <property>
-
       <name>dfs.replication</name>
-
       <value>1</value>
-
    </property>
-
-
    <property>
-
       <name>dfs.name.dir</name>
-
       <value>file:///home/hadoop/hdfs/namenode</value>
-
    </property>
-
-
    <property>
-
       <name>dfs.data.dir</name>
-
       <value>file:///home/hadoop/hdfs/datanode</value>
-
    </property> </configuration>"
 sudo echo $hdfs_site_content > "$HADOOP_HOME/etc/hadoop/hdfs-site.xml"
+echo "configured hdfs-site.xml"
 
-mapred_site_content="<configuration> <property>
 
-      <name>mapreduce.framework.name</name>
-
-      <value>yarn</value>
-
-   </property> </configuration>"
+mapred_site_content="<configuration> 
+ <property> 
+ <name>mapreduce.framework.name</name>  <value>yarn</value> 
+ </property> 
+ <property>
+ <name>mapreduce.application.classpath</name> 
+  
+<value>$HADOOP_MAPRED_HOME/share/hadoop/mapreduce/*:$HADOOP_MAPRED_HOME/share/hadoop/mapreduce/lib/*</value> 
+ </property> 
+</configuration>"
 sudo echo $mapred_site_content > $HADOOP_HOME/etc/hadoop/mapred-site.xml
+echo "configured mapred-site.xml"
+
 
 yarn_site_content="<configuration> 
-   <property>
-      <name>yarn.nodemanager.aux-services</name>
-      <value>mapreduce_shuffle</value>
-   </property> </configuration>"
+ <property> 
+ <name>yarn.nodemanager.aux-services</name> 
+ <value>mapreduce_shuffle</value> 
+ </property> 
+ <property> 
+ <name>yarn.nodemanager.env-whitelist</name> 
+  
+<value>JAVA_HOME,HADOOP_COMMON_HOME,HADOOP_HDFS_HOME,HADOOP_CONF_DIR,CLASSPATH_PREP END_DISTCACHE,HADOOP_YARN_HOME,HADOOP_MAPRED_HOME</value> 
+ </property> 
+</configuration>"
 sudo echo $yarn_site_content > $HADOOP_HOME/etc/hadoop/yarn-site.xml
+echo "configured yarn-site.xml"
 
-echo "Configuration is all set!"
+
 echo "Now please reboot your system"
-echo "Info: to start the framework: change into user hadoop: "
+echo "Info: to start the framework: change you need to be user hadoop: (run the command below if you are not user hadoop)"
 echo "sudo su - hadoop"
 echo "Then run start-all.sh to start the framework"
-echo "visit http://localhost:9870/ to view the web page"
+echo "You can visit http://localhost:9870/ to view the web page"
 echo "Info: run stop-all.sh to stop the hadoop framework"
 
 echo "running namenode format"
