32e9e1a155bbb02e8663ededd5aa66f6d06e7878
Nomaan Jamaluddin <nomaanjamaluddin@gmail.com>
2023-12-15 18:33:53 +0530

    Add Hadoop Scripts

---
+++ hadoop_stuff/dependencies-install.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+# VARIABLES
+HADOOP_VERSION="3.3.6"
+
+# Functions
+download_and_verify_hadoop() {
+
+  # Combine both checks into one conditional statement
+  if [[ ! -f /tmp/hadoop.tar.gz || $(stat -c %s /tmp/hadoop.tar.gz) -lt 500000000 ]]; then
+    echo "Downloading Hadoop..."
+    if ! wget -O /tmp/hadoop.tar.gz https://archive.apache.org/dist/hadoop/common/hadoop-${HADOOP_VERSION}/hadoop-${HADOOP_VERSION}.tar.gz; then
+      echo "Error: Downloading Hadoop failed."
+      echo "Please check your internet connection and re run the script."
+      rm /tmp/hadoop.tar.gz
+      exit 1
+    fi
+  else
+    echo "Hadoop archive already exists in /tmp and meets size requirements."
+  fi
+
+  # Additional file integrity checks (optional)
+  # You can add further checks here like checksum verification using tools like md5sum
+  # if [[ ! md5sum /tmp/hadoop.tar.gz | grep -q 'expected_checksum' ]]; then
+  #   echo "Error: File integrity mismatch."
+  #   rm /tmp/hadoop.tar.gz
+  #   exit 1
+  # fi
+
+  # File downloaded and verified successfully
+  echo "Hadoop downloaded and verified successfully."
+
+  # Extract Hadoop
+  if ! sudo tar xvf /tmp/hadoop.tar.gz -C /opt/; then
+    echo "Error: Extracting Hadoop failed."
+    echo "Is yoru Internet OK!?"
+    exit 1
+  fi
+
+  # Rename and change ownership
+  if ! sudo mv /opt/hadoop-${HADOOP_VERSION} /opt/hadoop && ! sudo chown -R hadoop:hadoop /opt/hadoop; then
+    echo "Error: Renaming or setting ownership failed."
+    exit 1
+  fi
+
+}
+
+clear
+sleep 2
+# Check if OpenSSH is installed
+if ! dpkg -l openssh-server | grep -q '^ii'; then
+  echo "Installing OpenSSH..."
+  sudo apt install openssh-client openssh-server -y
+else
+  echo "OpenSSH is already Installed"
+fi
+
+# Check if Java is installed
+if ! dpkg -l openjdk-11-jdk | grep -q '^ii'; then
+  echo "Installing Java..."
+  sudo apt install openjdk-11-jdk openjdk-11-jre -y
+else
+  echo "Java is already Installed!"
+fi
+
+# Check if user "hadoop" exists
+if ! id -u hadoop &> /dev/null; then
+  echo "Creating user 'hadoop'..."
+  sudo useradd -m hadoop
+  sudo usermod -aG sudo hadoop
+  echo "Set a password for the account hadoop"
+  sudo passwd hadoop
+else
+  echo "User 'hadoop' already exists."
+fi
+
+# Check if /opt/hadoop directory exists
+if [[ ! -d /opt/hadoop ]]; then
+  echo "Hadoop not installed or insufficient size, downloading..."
+  download_and_verify_hadoop
+else
+  echo "/opt/hadoop directory already exists and meets size requirements, skipping download."
+fi
+
+
+# Check if extraction directory exists
+if [[ ! -d /opt/hadoop ]]; then
+  echo "Error: Extraction failed or directory missing."
+  exit 1
+else
+  sudo mkdir -p /opt/hadoop/logs/
+fi
+echo "Hadoop successfully installed to /opt/hadoop"
+
+sudo chown -R hadoop:sudo /opt/hadoop
+
+# Cleanup (optional)
+# Add error checking to the `rm` command
+if [[ ! -f /tmp/hadoop.tar.gz ]]; then
+  echo "Info: No need to remove temporary file /tmp/hadoop.tar.gz."
+else
+  rm /tmp/hadoop.tar.gz
+  echo "Temporary file /tmp/hadoop.tar.gz removed."
+fi
+
+sleep 4
+clear
+echo "You will now be logged in as hadoop user please run the second script to automate hadoop configuration setup!"
+sleep 4
+
+# Change User to hadoop
+sudo su - hadoop
+
---
+++ hadoop_stuff/hadoop-configure.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+### Variables
+HADOOP_VERSION="3.3.6"
+
+### Setting up ssh client
+ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+sudo chmod 640 ~/.ssh/authorized_keys
+
+# Escape special characters in variable values
+HADOOP_HOME=/opt/hadoop
+HADOOP_INSTALL=$HADOOP_HOME
+HADOOP_MAPRED_HOME=$HADOOP_HOME
+HADOOP_COMMON_HOME=$HADOOP_HOME
+HADOOP_HDFS_HOME=$HADOOP_HOME
+YARN_HOME=$HADOOP_HOME
+HADOOP_COMMON_LIB_NATIVE_DIR="$HADOOP_HOME/lib/native"
+PATH="$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin"
+
+# Check if the .bashrc file is writable
+if [[ -w ~/.bashrc ]]; then
+  # Append the export lines to the .bashrc file
+  echo "export HADOOP_HOME=$HADOOP_HOME" >> ~/.bashrc
+  echo "export HADOOP_INSTALL=$HADOOP_INSTALL" >> ~/.bashrc
+  echo "export HADOOP_MAPRED_HOME=$HADOOP_HOME" >> ~/.bashrc
+  echo "export HADOOP_COMMON_HOME=$HADOOP_HOME" >> ~/.bashrc
+  echo "export HADOOP_HDFS_HOME=$HADOOP_HOME" >> ~/.bashrc
+  echo "export YARN_HOME=$HADOOP_HOME" >> ~/.bashrc
+  echo "export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native" >> ~/.bashrc
+  echo "export PATH=$PATH" >> ~/.bashrc
+  echo "Hadoop environment variables added to ~/.bashrc."
+else
+  echo "Error: ~/.bashrc is not writable. Please fix permissions or use a different method."
+fi
+
+### Configuring Apache Hadoop
+
+hadoop_env_content="export JAVA_HOME="/usr/lib/jvm/default-java/""  
+hadoop_class_path="export HADOOP_CLASSPATH+="'$HADOOP_HOME/lib/*.jar'""
+sudo echo $hadoop_env_content > $HADOOP_HOME/etc/hadoop/hadoop-env.sh
+sudo echo $hadoop_class_path >> $HADOOP_HOME/etc/hadoop/hadoop-env.sh
+
+xml_content="<configuration> <property>
+
+      <name>fs.default.name</name>
+
+      <value>hdfs://0.0.0.0:9000</value>
+
+      <description>The default file system URI</description>
+
+   </property> </configuration>"
+sudo echo $xml_content > $HADOOP_HOME/etc/hadoop/core-site.xml
+
+sudo mkdir -p /home/hadoop/hdfs/{namenode,datanode}
+sudo chown -R hadoop:hadoop /home/hadoop/hdfs
+
+hdfs_site_content="<configuration> <property>
+
+      <name>dfs.replication</name>
+
+      <value>1</value>
+
+   </property>
+
+
+   <property>
+
+      <name>dfs.name.dir</name>
+
+      <value>file:///home/hadoop/hdfs/namenode</value>
+
+   </property>
+
+
+   <property>
+
+      <name>dfs.data.dir</name>
+
+      <value>file:///home/hadoop/hdfs/datanode</value>
+
+   </property> </configuration>"
+sudo echo $hdfs_site_content > "$HADOOP_HOME/etc/hadoop/hdfs-site.xml"
+
+mapred_site_content="<configuration> <property>
+
+      <name>mapreduce.framework.name</name>
+
+      <value>yarn</value>
+
+   </property> </configuration>"
+sudo echo $mapred_site_content > $HADOOP_HOME/etc/hadoop/mapred-site.xml
+
+yarn_site_content="<configuration> 
+   <property>
+      <name>yarn.nodemanager.aux-services</name>
+      <value>mapreduce_shuffle</value>
+   </property> </configuration>"
+sudo echo $yarn_site_content > $HADOOP_HOME/etc/hadoop/yarn-site.xml
+
+echo "Configuration is all set!"
+echo "Now please reboot your system"
+echo "Info: to start the framework: change into user hadoop: "
+echo "sudo su - hadoop"
+echo "Then run start-all.sh to start the framework"
+echo "visit http://localhost:9870/ to view the web page"
+echo "Info: run stop-all.sh to stop the hadoop framework"
+
+echo "running namenode format"
+hdfs namenode -format
+ 
+
